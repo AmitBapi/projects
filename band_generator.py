6b5dbcedbf89eefe1a4c61ab157ba73f22419c31
AmitBapi <amitbapi928@gnmail.com>
2025-01-04 17:40:03 +0530

    created band generator

--- band_generator.py
+++ band_generator.py
@@ -1,4 +1,4 @@
 print("Auto Band Name Generator")
 city_name= input("Enter Your City Name = ")
 wished_name = input("Enter Any Wished Name = ")
-print("Your Band Name Could Be: " + city_name + " " + wished_name)
+print("Your Band Name Could Be : " + city_name + " " + wished_name)
